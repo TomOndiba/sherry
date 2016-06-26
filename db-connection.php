08440c3a1418e7f7b10b1cd293a6f589deed53d3
shehrozsheikh <shehrozsheikh@outlook.com>
2016-06-26 17:21:48 +0500

    Update db-connection.php

--- db-connection.php
+++ db-connection.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect("sql7.freemysqlhosting.net", "sql7111401", "GmslwgEXpK", "sql7111401");
+$link = mysqli_connect("sql7.freemysqlhosting.net", "sql5125470", "cWpaIFWe5C", "sql5125470");
 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
